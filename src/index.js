525f496f8574310dd1b01b96fde6f6e544fe7ac2
Thong Phung <thongphungvan14@gmail.com>
2021-11-26 13:55:22 +0700

    sua dependencies

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 const route = require('./routes')
 
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 app.use(express.static(path.join(__dirname, '/public')))
 
